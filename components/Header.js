18b14da571fd0364c0a4cce48ae8b2fe56b74eca
HaileyPong <haileypong@berkeley.edu>
2023-09-19 10:44:01 -0400

    fixed nav tab location issue

--- components/Header.js
+++ components/Header.js
@@ -1,80 +1,112 @@
-
 "use client";
-import { React, useState } from 'react';
+import { React, useState } from "react";
 import Link from "next/link";
-import styles from '../styles/header.module.css';
+import styles from "../styles/header.module.css";
 import DarkMode from "./darkMode";
 import Image from "next/image";
 
-
-  
-
 const Header = () => {
-	const [showOptions, setShowOptions] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
 
-	const toggleOptions = () => {
-		setShowOptions(!showOptions);
-	};
-const links = [
-	{
-	  id: 1,
-	  title: <Image src="/3.png" width={15} height={15} className={styles.icon} alt="kimchi Twitter" />,
-	  url: "/",
-	},
-	{
-	  id: 2,
-	  title: <Image src="/discord.png" width={25} height={25} className={styles.icon} alt="kimchi discord" />,
-	  url: "/discord",
-	},
-	{
-	  id: 3,
-	  title: <Image src="/telegram.webp" width={15} height={15} className={styles.icon} alt="kimchi telegram" />,
-	  url: "/telegram",
-	},
-	{
-		id: 4,
-		title: (
-		  <div
-			onClick={toggleOptions}
-			className={`transaction-tab ${showOptions ? 'active' : ''}`}
-		  >
-			Transactions
-			{showOptions && (
-			  <div className={`transaction-options ${showOptions ? 'show' : ''}`}>
-				<p>Klaytn</p>
-				<p>WeMix</p>
-			  </div>
-			)}
-		  </div>
-		),
-		url: "/tx",
-	},
-	{
-	  id: 5,
-	  title: "Contact",
-	  url: "/contact",
-	},
-	
+  const toggleOptions = () => {
+    setShowOptions(!showOptions);
+  };
+  const links = [
+    {
+      id: 1,
+      title: (
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="kimchi Twitter"
+        />
+      ),
+      url: "/",
+    },
+    {
+      id: 2,
+      title: (
+        <Image
+          src="/discord.png"
+          width={25}
+          height={25}
+          className={styles.icon}
+          alt="kimchi discord"
+        />
+      ),
+      url: "/discord",
+    },
+    {
+      id: 3,
+      title: (
+        <Image
+          src="/telegram.webp"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="kimchi telegram"
+        />
+      ),
+      url: "/telegram",
+    },
+    {
+      id: 4,
+      title: (
+        <div
+          onClick={toggleOptions}
+          className={`${styles["transaction-tab"]} ${
+            showOptions ? styles.active : ""
+          }`}
+        >
+          Transactions
+          {showOptions && (
+            <div
+              className={`${styles["transaction-options"]} ${
+                showOptions ? "show" : ""
+              }`}
+            >
+              <Link href={"./"}>
+                <p>Klaytn</p>
+              </Link>
+              <Link href={"./"}>
+                <p>WeMix</p>
+              </Link>
+            </div>
+          )}
+        </div>
+      ),
+      url: "/tx",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
   ];
 
   return (
-	
-	<div className={styles.container}>
-	<Image src="/kimchi.png" width={110} height={75} className={styles.icon}alt="uluwatu lab" />
-	<div className={styles.links}>
-	<DarkMode />
-	  {links.map((link) => (
-		<Link key={link.id} href={link.url} className={styles.links}>
-		  {link.title}
-		</Link>
-	  ))}
-	 
-	</div>
-  </div>
- 
-
-  )
-}
-
+    <div className={styles.container}>
+      <a href="/" title="Go to Home">
+        <Image
+          src="/kimchi.png"
+          width={110}
+          height={75}
+          className={styles.icon}
+          alt="uluwatu lab"
+        />
+      </a>
+      <div className={styles.links}>
+        <DarkMode />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- pages/tx.js
+++ pages/tx.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import styles from '../styles/tx.module.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import styles from "../styles/tx.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const TX = () => {
-  
-	return (
-		
-		<div className={styles['card-container']}>
-			 <Header />
-		  <div className={`${styles.card} ${styles.transactionCard}`}>
-		  <h2>Transaction Details</h2>
-        <p>
-          <strong>Blockchain:</strong> Ethereum
-        </p>
-        <p>
-          <strong>Timestamp:</strong> 2 days 10 hours ago (Sat, 16 Sep 2023 10:58:59 UTC)
-        </p>
-        <p>
-          <strong>Hash:</strong> 0x36a4bf7781b994c413a39ea1850916c960c18e2114f12fe1765c637d0af4265b
-        </p>
-        <p>
-          <strong>Fee:</strong> 0.000208649178918525 ETH (0.34 USD)
-        </p>
-		  </div>
-		  <div className={`${styles.card} ${styles.transferCard}`}>
-		  <h2>Transfer</h2>
-        <p>
-          <strong>Sender:</strong> 0x49a1a7f6...696f4b7c26 22,343 ETH
-        </p>
-        <p>
-          <strong>Receiver:</strong> 0xa9d1e08c...7fb81d3e43 Coinbase Warning 22,343 ETH
-        </p>
-		  </div>
+  return (
+    <>
+      <Header />
+      <div className={styles["card-container"]}>
+        <div className={`${styles.card} ${styles.transactionCard}`}>
+          <h2>Transaction Details</h2>
+          <p>
+            <strong>Blockchain:</strong> Ethereum
+          </p>
+          <p>
+            <strong>Timestamp:</strong> 2 days 10 hours ago (Sat, 16 Sep 2023
+            10:58:59 UTC)
+          </p>
+          <p>
+            <strong>Hash:</strong>{" "}
+            0x36a4bf7781b994c413a39ea1850916c960c18e2114f12fe1765c637d0af4265b
+          </p>
+          <p>
+            <strong>Fee:</strong> 0.000208649178918525 ETH (0.34 USD)
+          </p>
+        </div>
+        <div className={`${styles.card} ${styles.transferCard}`}>
+          <h2>Transfer</h2>
+          <p>
+            <strong>Sender:</strong> 0x49a1a7f6...696f4b7c26 22,343 ETH
+          </p>
+          <p>
+            <strong>Receiver:</strong> 0xa9d1e08c...7fb81d3e43 Coinbase Warning
+            22,343 ETH
+          </p>
+        </div>
 
-		  <Footer />
-		</div>
-	  );
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default TX;

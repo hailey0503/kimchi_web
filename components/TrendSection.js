d76f556f1dd2f0869e12db00a1c938a661d8c0d8
HaileyPong <haileypong@berkeley.edu>
2023-11-23 18:31:32 -0500

    changed env variable

---
+++ components/TrendSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/trendSection.module.css';
+
+const TrendSection = () => {
+  return (
+    <section className={styles.mission}>
+      <h2>Our Mission</h2>
+    
+    </section>
+  );
+};
+
+export default TrendSection;
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import AboutUsSection from '../components/AboutUsSection';
-import MissionSection from '../components/MissionSection';
+import MissionSection from '../components/TrendSection';
 import ProductSection from '../components/ProductSection';
 import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
+import TrendSection from '../components/TrendSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <ProductSection />
+    
       <ContactSection />
       <Footer />
     </div>
